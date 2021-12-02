04e70dd060f4a3684c3fa6b894af0db7028de2e1
ElenaCherpakova <elena.cherpakova@gmail.com>
2021-12-02 05:59:51 +0000

    pairprogramming

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,35 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  const transposedMatrix = transpose(letters);
+  const horizontalJoin = transposedMatrix.map(ls => ls.join(''));
+  const verticalJoin = letters.map(vs => vs.join(''));
+  if (letters.length === 0) {
+    return false;
+  }
+  for (l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  for (v of verticalJoin) {
+    if (v.includes(word)) return true;
+  }
+  return false;
+};
+  
+  
+const transpose = function(letters) {
+  let rowArr = [];
+  let resultArr = [];
+  if (letters.length !== 0) {
+    for (let c = 0; c < letters[0].length; c++) {
+      for (let r = 0; r < letters.length; r++) {
+        rowArr.push(letters[r][c]);
+      }
+      resultArr.push(rowArr);
+      rowArr = [];
     }
-}
-
-module.exports = wordSearch
+    return resultArr;
+  }
+  return [];
+};
+  
+  
+module.exports = wordSearch;
